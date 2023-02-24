6897c8d080655339ee608987163c0d76ffe223c8
Vincent Fuertez <vfuertez@gmail.com>
2023-02-24 02:28:43 -0500

    added viewset, url routers

--- project/views.py
+++ project/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Proj
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import ProjSerializer
+
+class ProjViewset(viewsets.ModelViewSet):
+    queryset = Proj.objects.all()
+    serializer_class = ProjSerializer
+    permission_classes = [permissions.AllowAny]
--- projectfour/urls.py
+++ projectfour/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from project.views import ProjViewset
+
+# creates a router
+router = routers.DefaultRouter()
+
+# register our viewsets
+router.register(r'project', ProjViewset) #register "/todos" routes
 
 urlpatterns = [
+    # add all of our router urls
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
